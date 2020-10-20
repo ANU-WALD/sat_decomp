2cac83b661df976071ac7fe6ba981610da629b72
prl900 <pablo.larraondo@anu.edu.au>
2020-10-20 14:21:35 +1100

    map comparison between products

--- wms/server.py
+++ wms/server.py
@@ -6,7 +6,9 @@ from flask import make_response
 
 from pyproj import Proj, Transformer
 import imageio
+from matplotlib import cm
 import numpy as np
+import xarray as xr
 import math
 #import numexpr as ne
 
@@ -24,23 +26,43 @@ pca_coeffs = "../data/%02d_%02d_pca_coeffs.npy"
 pca_pcs = "../data/%02d_%02d_pca_pcs.npy"
 pca_mean = "../data/%02d_%02d_pca_mean.npy"
 
+n_bands = 7
+
+tile_size = 400
+
+
 app = Flask(__name__)
 
+def get_original_tile(i, j, t):
+    ds = xr.open_dataset(f"/data/pca_act/{26*i+j:03d}_2018.nc")
+    #ds2019 = xr.open_dataset(f"/data/pca_act/{26*j+i:03d}_2019.nc")
+    #ds = xr.concat([ds2018, ds2019], dim='time').sortby('time')
 
-def get_tile(i, j, t):
+
+    r = ds.nbart_red.isel(time=t).values/10000
+    #g = ds.nbart_green.isel(time=0).values/10000
+    #b = ds.nbart_blue.isel(time=0).values/10000
+    nir = ds.nbart_nir_1.isel(time=t).values/10000
+
+    return (nir-r)/(nir+r)
+    return np.dstack((r,g,b))
+
+def get_compressed_tile(i, j, t):
     coeffs = np.load(pca_coeffs%(i,j))
-    pcs = np.load(pca_pcs%(i,j)).reshape(-1,160000)
+    pcs = np.load(pca_pcs%(i,j)).reshape(-1,tile_size*tile_size)
     mean = np.load(pca_mean%(i,j))
-    stride = coeffs.shape[0]//7
+    stride = coeffs.shape[0]//n_bands
 
-    r = mean+(coeffs[None,t,:].dot(pcs)).reshape(400,400)
-    g = mean+(coeffs[None,t+stride,:].dot(pcs)).reshape(400,400)
-    b = mean+(coeffs[None,t+2*stride,:].dot(pcs)).reshape(400,400)
-    
+    r = mean+(coeffs[None,t+0*stride,:].dot(pcs)).reshape(tile_size,tile_size)
+    #g = mean+(coeffs[None,t+1*stride,:].dot(pcs)).reshape(tile_size,tile_size)
+    #b = mean+(coeffs[None,t+2*stride,:].dot(pcs)).reshape(tile_size,tile_size)
+    nir = mean+(coeffs[None,t+3*stride,:].dot(pcs)).reshape(tile_size,tile_size)
+
+    return (nir-r)/(nir+r)
     return np.dstack((r,g,b))
 
 
-def bbox2tile(bbox, band, t, im_size, proj):
+def bbox2tile(bbox, layer, band, t, im_size, proj):
     trans = Transformer.from_crs(proj, aaea_proj)
 
     x_tl, y_tl = trans.transform(bbox[0], bbox[3])
@@ -61,7 +83,7 @@ def bbox2tile(bbox, band, t, im_size, proj):
     arr = None
     for i in range(x0, x1):
         for j in range(y0, y1):
-            a = get_partial_tile(bbox, band, i, j, t, im_size, proj)
+            a = get_partial_tile(bbox, layer, band, i, j, t, im_size, proj)
             if arr is None:
                 arr = a
                 continue
@@ -70,17 +92,10 @@ def bbox2tile(bbox, band, t, im_size, proj):
     
     return arr
 
-
-def xy2ij(origin, pixel_size, x, y):
-    i = round((x - origin[0]) / 10.0)
-    j = round((origin[1] - y) / 10.0)
-    
-    return i, j
-    
-def get_partial_tile(bbox, b, i, j, t, im_size=256, proj=wgs84_proj):
+def get_partial_tile(bbox, layer, b, i, j, t, im_size=256, proj=wgs84_proj):
     pixel_size = ((bbox[2] - bbox[0]) / im_size, (bbox[3] - bbox[1]) / im_size)
 
-    arr = np.zeros((im_size,im_size,3), dtype=np.float32)
+    #arr = np.zeros((im_size,im_size,3), dtype=np.float32)
     
     lons = []
     lats = []
@@ -91,26 +106,27 @@ def get_partial_tile(bbox, b, i, j, t, im_size=256, proj=wgs84_proj):
     
     trans = Transformer.from_crs(proj, aaea_proj)
     xs, ys = trans.transform(lons, lats)
-
-    print(xs[0], xs[-1], ys[0], ys[-1], len(xs))
-   
-    tile = get_tile(i, j, t)
-
+    
     origin = (1506645.0+i*4000, -3932965.0-j*4000)
+    arr = np.zeros((im_size,im_size), dtype=np.float32)
+
+    if layer == 'orig':
+        tile = get_original_tile(i, j, t)
+    elif layer == 'comp':
+        tile = get_compressed_tile(i, j, t)
+    else:
+        return arr
 
     for j in range(im_size):
         for i in range(im_size):
-            oi, oj = xy2ij(origin, (10.0,-10.0), xs[j*im_size+i], ys[j*im_size+i])
+            oi = round((xs[j*im_size+i] - origin[0]) / 10.0)
+            oj = round((origin[1] - ys[j*im_size+i]) / 10.0)
 
-            if oi > 399 or oj > 399:
-                #arr[j,i] = np.nan
-                #arr[j,i] = 0
+            if oi < 0 or oj < 0 or oi > 399 or oj > 399:
                 continue
-            if oi < 0 or oj < 0:
-                #arr[j,i] = np.nan
-                #arr[j,i] = 0
-                continue
-            arr[j,i,:] = tile[oj,oi,:]  
+            
+            #arr[j,i,:] = tile[oj,oi,:]  
+            arr[j,i] = tile[oj,oi]  
             
     return arr
 
@@ -118,35 +134,38 @@ def get_partial_tile(bbox, b, i, j, t, im_size=256, proj=wgs84_proj):
 
 @app.route('/wms')
 def wms():
+
+    print(request.url)
+
     service = request.args.get('service')
     if service != 'WMS':
-        return "Malformed request: only WMS requests implemented", 400
-
+        return "Malformed request: only WMS service implemented", 400
+    
     req = request.args.get('request')
-    if req == 'GetCapabilities':
-        layers = [{'name': 'NDVI', 'title': 'Dynamic NDVI', 'abstract': 'AI generated'}]
-        template = render_template('GetCapabilities.xml', layers=layers)
-        response = make_response(template)
-        response.headers['Content-Type'] = 'application/xml'
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        return response
-
     if req != 'GetMap':
-        return "Malformed request: only GetMap and GetCapabilities requests implemented", 400
+        return "Malformed request: only GetMap requests implemented", 400
 
+    layer = request.args.get('layers', default='comp')
+    
     bbox = request.args.get('bbox').split(',')
     if len(bbox) != 4:
         return "Malformed request: bbox must have 4 values", 400
     bbox = [float(p) for p in bbox]
+   
+    t_idx = request.args.get('time', type=int, default=None)
+    if t_idx is None:
+        return "Malformed request: request need a time", 400
 
     width = int(request.args.get('width'))
     height = int(request.args.get('height'))
     srs = request.args.get('srs').lower()
 
-    im = bbox2tile(bbox, 1, 0, width, srs)
-    im = np.clip(im, 0, 0.3643)
-    im *= 700
-    im = im.astype(np.uint8)
+    im = bbox2tile(bbox, layer, 1, t_idx, width, srs)
+    im = np.uint8(cm.summer_r(im)*255)
+    
+    #im = np.clip(im, 0, 0.3643)
+    #im *= 700
+    #im = im.astype(np.uint8)
 
     out = io.BytesIO()
     imageio.imwrite(out, im, format='png') 
