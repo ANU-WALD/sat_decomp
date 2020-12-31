02f179ae4c7e384b5f90a5f85a8bc0d0bdc46ca0
prl900 <pablo.larraondo@anu.edu.au>
2020-12-31 21:32:34 +1100

    measuring sparsity

--- exp1p.py
+++ exp1p.py
@@ -53,6 +53,37 @@ for j in range(18):
 
 
         stack = stack.reshape(-1, 160000) / 10000
+
+        pca = PCA(n_components=ncomps).fit(stack)
+        coeffs = pca.transform(stack)
+        pca_decomp = pca.inverse_transform(coeffs)
+
+        print("PCA MSE 12 comp", np.mean(np.square(pca_decomp - stack)))
+        
+        pca = PCA(n_components=11).fit(stack)
+        coeffs = pca.transform(stack)
+        pca_decomp = pca.inverse_transform(coeffs)
+
+        print("PCA MSE 11 comp", np.mean(np.square(pca_decomp - stack)))
+
+        pca = PCA(n_components=10).fit(stack)
+        coeffs = pca.transform(stack)
+        pca_decomp = pca.inverse_transform(coeffs)
+
+        print("PCA MSE 10 comp", np.mean(np.square(pca_decomp - stack)))
+
+        pca = PCA(n_components=9).fit(stack)
+        coeffs = pca.transform(stack)
+        pca_decomp = pca.inverse_transform(coeffs)
+
+        print("PCA MSE 9 comp", np.mean(np.square(pca_decomp - stack)))
+
+        pca = PCA(n_components=8).fit(stack)
+        coeffs = pca.transform(stack)
+        pca_decomp = pca.inverse_transform(coeffs)
+
+        print("PCA MSE 8 comp", np.mean(np.square(pca_decomp - stack)))
+
         stack_mean = np.mean(stack, axis=0)
         target = torch.from_numpy(stack-stack_mean).float().to(device)
 
@@ -104,7 +135,12 @@ for j in range(18):
             net.cfs.data = net.cfs.data*torch.norm(net.cmps, dim=1).data/20
             net.cmps.data = net.cmps.data/torch.norm(net.cmps, dim=1).data[:,None]*20
             
-        plt.imshow(net.cfs.detach().cpu().numpy().T, vmin=-1.0, vmax=1.0, aspect=10, cmap='bwr')
+        cfs = net.cfs.detach().cpu().numpy()
+        print("cfs size:", cfs.shape, cfs.size)
+        print("Zeros before:", np.sum(np.isclose(cfs, np.zeros(cfs.shape), rtol=1e-03, atol=1e-04)))
+        print(f"Zeros after: 0.1->{np.sum(np.abs(cfs)<0.1)} 0.01->{np.sum(np.abs(cfs)<0.01)} 0.001->{np.sum(np.abs(cfs)<0.001)} 0.0001->{np.sum(np.abs(cfs)<0.0001)} 0.00001->{np.sum(np.abs(cfs)<0.00001)} 0.000001->{np.sum(np.abs(cfs)<0.000001)} 0.0000001->{np.sum(np.abs(cfs)<0.0000001)}")
+
+        plt.imshow(cfs.T, vmin=-1.0, vmax=1.0, aspect=10, cmap='bwr')
         plt.colorbar()
         plt.savefig(f"coeffs_{26*j+i:03d}_dense.png")
         plt.clf()
@@ -121,9 +157,43 @@ for j in range(18):
             if epoch % 100 == 0:
                 print(epoch, torch.norm(net.cfs, p=1).item(), mse(yhat, target).item(), unit_norm(net.cmps).item())
 
-        plt.imshow(net.cfs.detach().cpu().numpy().T, vmin=-1.0, vmax=1.0, aspect=10, cmap='bwr')
+        cfs = net.cfs.detach().cpu().numpy()
+        print("Zeros after:", np.sum(np.isclose(cfs, np.zeros(cfs.shape), rtol=1e-03, atol=1e-04)))
+        print(f"Zeros after: 0.1->{np.sum(np.abs(cfs)<0.1)} 0.01->{np.sum(np.abs(cfs)<0.01)} 0.001->{np.sum(np.abs(cfs)<0.001)} 0.0001->{np.sum(np.abs(cfs)<0.0001)} 0.00001->{np.sum(np.abs(cfs)<0.00001)} 0.000001->{np.sum(np.abs(cfs)<0.000001)} 0.0000001->{np.sum(np.abs(cfs)<0.0000001)}")
+        plt.imshow(cfs.T, vmin=-1.0, vmax=1.0, aspect=10, cmap='bwr')
         plt.colorbar()
         plt.savefig(f"coeffs_{26*j+i:03d}_sparse.png")
         plt.clf()
 
 
+        opt = optim.AdamW(net.parameters(), lr=0.01)
+        n_epoch  = 2000
+        for epoch in range(n_epoch):
+            yhat = net()
+            loss = mse(yhat, target) + λ*unit_norm(net.cmps) + 2*μ*torch.norm(net.cfs, p=1)
+            net.zero_grad() # need to clear the old gradients
+            loss.backward()
+            opt.step()
+            
+            if epoch % 100 == 0:
+                print(epoch, torch.norm(net.cfs, p=1).item(), mse(yhat, target).item(), unit_norm(net.cmps).item())
+
+        cfs = net.cfs.detach().cpu().numpy()
+        print("Zeros after 2x:", np.sum(np.isclose(cfs, np.zeros(cfs.shape), rtol=1e-03, atol=1e-04)))
+        print(f"Zeros after 2x: 0.1->{np.sum(np.abs(cfs)<0.1)} 0.01->{np.sum(np.abs(cfs)<0.01)} 0.001->{np.sum(np.abs(cfs)<0.001)} 0.0001->{np.sum(np.abs(cfs)<0.0001)} 0.00001->{np.sum(np.abs(cfs)<0.00001)} 0.000001->{np.sum(np.abs(cfs)<0.000001)} 0.0000001->{np.sum(np.abs(cfs)<0.0000001)}")
+        
+        opt = optim.AdamW(net.parameters(), lr=0.01)
+        n_epoch  = 2000
+        for epoch in range(n_epoch):
+            yhat = net()
+            loss = mse(yhat, target) + λ*unit_norm(net.cmps) + 4*μ*torch.norm(net.cfs, p=1)
+            net.zero_grad() # need to clear the old gradients
+            loss.backward()
+            opt.step()
+            
+            if epoch % 100 == 0:
+                print(epoch, torch.norm(net.cfs, p=1).item(), mse(yhat, target).item(), unit_norm(net.cmps).item())
+
+        cfs = net.cfs.detach().cpu().numpy()
+        print("Zeros after 4x:", np.sum(np.isclose(cfs, np.zeros(cfs.shape), rtol=1e-03, atol=1e-04)))
+        print(f"Zeros after 4x: 0.1->{np.sum(np.abs(cfs)<0.1)} 0.01->{np.sum(np.abs(cfs)<0.01)} 0.001->{np.sum(np.abs(cfs)<0.001)} 0.0001->{np.sum(np.abs(cfs)<0.0001)} 0.00001->{np.sum(np.abs(cfs)<0.00001)} 0.000001->{np.sum(np.abs(cfs)<0.000001)} 0.0000001->{np.sum(np.abs(cfs)<0.0000001)}")
